2c106ce6206bfee9aa5e7065c2df9b7ae0faa6f1
tianhuifeis <tianhuifeis@126.com>
2017-12-07 17:49:23 +0800

    增加检验项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'eol-last': 0,
-    'space-before-function-paren': 0
+    'eol-last':0,
+    'space-before-function-paren':0
   }
 }
