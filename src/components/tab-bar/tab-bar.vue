9ced6b2c8e0fbad57ffe5f6285e29d3c1cc0f9e7
tianhuifeis <tianhuifeis@126.com>
2017-12-08 14:46:39 +0800

    导航组件

---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tab-bar">
+    <div class="tab-item"></div>
+    <div class="tab-item"></div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  @import "../../assets/css/base.scss";
+  .tab-bar{
+    height:50px;
+    background-color:$tab-bar-bg-color;
+    position:fixed;
+    bottom:0;
+    width:100%;
+    display: flex;
+    border-top:1px solid #EEE;
+    box-shadow: 0 0 1px rgba(0,0,0,.85);
+    .tab-item{
+      flex:1;
+    }
+  }
+</style>
