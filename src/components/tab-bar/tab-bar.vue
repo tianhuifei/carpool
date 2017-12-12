ac7d4ea7258249ea6dbece9376dcc3dbe6bf5fb0
tianhuifeis <tianhuifeis@126.com>
2017-12-12 10:47:06 +0800

    tab-bar开发

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="tab-bar">
     <div class="tab-item">
-      <router-link to="/foo">
-        <span class="carpoll car-wode4"></span>
-        <span>2</span>
+      <router-link to="/foo" >
+        <span class="carpoll car-shouye"></span>
+        <span class="tab-text">首页</span>
       </router-link>
     </div>
     <div class="tab-item">
       <router-link to="/foo">
         <span class="carpoll car-wode4"></span>
-        <span>2</span>
+        <span class="tab-text">我的</span>
       </router-link>
     </div>
   </div>
@@ -31,7 +31,22 @@
     border-top:1px solid #EEE;
     box-shadow: 0 0 1px rgba(0,0,0,.85);
     .tab-item{
-      flex:1;
+      flex: 1;
+      >a{
+        display: block;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        .carpoll{
+          font-size:20px;
+        }
+        .tab-text{
+          font-size:12px;
+          margin-top:5px;
+        }
+      }
     }
   }
 </style>
