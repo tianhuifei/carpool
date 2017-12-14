c68cbc8d024142c0c57b25c3149cb7f191b90cfa
tianhuifeis <tianhuifeis@126.com>
2017-12-14 16:48:01 +0800

    slide开发

--- src/base/slide/slide.vue
+++ src/base/slide/slide.vue
@@ -1,16 +1,116 @@
 <template>
-    <div class="slider">
-        <slot></slot>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
     </div>
     <div class="dots"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        name: "slide"
+  import BScroll from 'better-scroll'
+  import {addClass} from 'common/js/dom'
+
+  export default {
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initSlider()
+      }, 20)
+    },
+    methods: {
+      _setSliderWidth() {
+        this.children = this.$refs.sliderGroup.children
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slide-item')
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop) {
+          width += sliderWidth * 2
+        }
+
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          momentum: false,
+          snap: {
+            loop: this.loop,
+            threshold: 0.3,
+            speed: 400
+          }
+        })
+      }
     }
+  }
 </script>
 
 <style scoped lang="scss">
+  @import "~assets/css/base-color";
 
+  .slider {
+    min-height: 1px;
+    .slider-group {
+      position: relative;
+      overflow: hidden;
+      white-space: nowrap;
+      .slide-item {
+        float: left;
+        box-sizing: border-box;
+        overflow: hidden;
+        text-align: center;
+        a {
+          display: block;
+          width: 100%;
+          overflow: hidden;
+          text-decoration: none;
+        }
+        img {
+          display: block;
+          width: 100%;
+        }
+      }
+    }
+    .dots {
+      position: absolute;
+      right: 0;
+      left: 0;
+      bottom: 12px;
+      transform: translateZ(1px);
+      text-align: center;
+      font-size: 0;
+      .dot {
+        display: inline-block;
+        margin: 0 4px;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: $color-light-grey-sss;
+        &.active {
+          width: 20px;
+          border-radius: 5px;
+          background-color: $color-white;
+        }
+      }
+    }
+  }
 </style>
