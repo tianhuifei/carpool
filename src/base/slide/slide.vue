7c0f027763c3a6ced17cce01ee54e263fcd77564
tianhuifeis <tianhuifeis@126.com>
2017-12-16 21:01:13 +0800

    更新slide组件

--- src/base/slide/slide.vue
+++ src/base/slide/slide.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="slider" ref="slider">
     <div class="slider-group" ref="sliderGroup">
-      <slot></slot>
+      <slot>
+      </slot>
     </div>
     <div class="dots">
-      <span class="dot" v-for="(item, index) in dots" :class="{ active : currentPageIndex === index }"></span>
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots"></span>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import BScroll from 'better-scroll'
   import {addClass} from 'common/js/dom'
-
+  import BScroll from 'better-scroll'
   export default {
-    data() {
-      return {
-        dots: [],
-        currentPageIndex: 0
-      }
-    },
+    name: 'slider',
     props: {
       loop: {
         type: Boolean,
@@ -34,44 +29,81 @@
         default: 4000
       }
     },
+    data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
     mounted() {
       setTimeout(() => {
         this._setSliderWidth()
         this._initDots()
         this._initSlider()
-
         if (this.autoPlay) {
           this._play()
         }
       }, 20)
       window.addEventListener('resize', () => {
-        if (this.slider) {
+        if (!this.slider || !this.slider.enabled) {
           return
         }
-        this._setSliderWidth(true)
-        this.slider.refresh()
+        clearTimeout(this.resizeTimer)
+        this.resizeTimer = setTimeout(() => {
+          if (this.slider.isInTransition) {
+            this._onScrollEnd()
+          } else {
+            if (this.autoPlay) {
+              this._play()
+            }
+          }
+          this.refresh()
+        }, 60)
       })
     },
+    activated() {
+      this.slider.enable()
+      let pageIndex = this.slider.getCurrentPage().pageX
+      this.slider.goToPage(pageIndex, 0, 0)
+      this.currentPageIndex = pageIndex
+      if (this.autoPlay) {
+        this._play()
+      }
+    },
+    deactivated() {
+      this.slider.disable()
+      clearTimeout(this.timer)
+    },
+    beforeDestroy() {
+      this.slider.disable()
+      clearTimeout(this.timer)
+    },
     methods: {
+      refresh() {
+        if (this.slider) {
+          this._setSliderWidth(true)
+          this.slider.refresh()
+        }
+      },
       _setSliderWidth(isResize) {
         this.children = this.$refs.sliderGroup.children
         let width = 0
         let sliderWidth = this.$refs.slider.clientWidth
         for (let i = 0; i < this.children.length; i++) {
           let child = this.children[i]
-          addClass(child, 'slide-item')
+          addClass(child, 'slider-item')
           child.style.width = sliderWidth + 'px'
           width += sliderWidth
         }
         if (this.loop && !isResize) {
-          width += sliderWidth * 2
+          width += 2 * sliderWidth
         }
-
         this.$refs.sliderGroup.style.width = width + 'px'
       },
       _initSlider() {
         this.slider = new BScroll(this.$refs.slider, {
           scrollX: true,
+          scrollY: false,
           momentum: false,
           snap: {
             loop: this.loop,
@@ -79,27 +111,32 @@
             speed: 400
           }
         })
-        this.slider.on('scrollEnd', () => {
-          let pageIndex = this.slider.getCurrentPage().pageX
-          if (this.loop) {
-            pageIndex -= 1
-          }
-          this.currentPageIndex = pageIndex
-
+        this.slider.on('scrollEnd', this._onScrollEnd)
+        this.slider.on('touchend', () => {
           if (this.autoPlay) {
-            clearTimeout(this.timer)
             this._play()
           }
         })
+        this.slider.on('beforeScrollStart', () => {
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+          }
+        })
+      },
+      _onScrollEnd() {
+        let pageIndex = this.slider.getCurrentPage().pageX
+        this.currentPageIndex = pageIndex
+        if (this.autoPlay) {
+          this._play()
+        }
       },
       _initDots() {
         this.dots = new Array(this.children.length)
       },
       _play() {
-        let pageIndex = this.slider.getCurrentPage().pageX + 1
         clearTimeout(this.timer)
         this.timer = setTimeout(() => {
-          this.slider.goToPage(pageIndex, 0, 400)
+          this.slider.next()
         }, this.interval)
       }
     }
@@ -115,7 +152,7 @@
       position: relative;
       overflow: hidden;
       white-space: nowrap;
-      .slide-item {
+      .slider-item {
         float: left;
         box-sizing: border-box;
         overflow: hidden;
