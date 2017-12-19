ac92f6a9076945060b9f26be8eaf421c2565875a
tianhuifeis <tianhuifeis@126.com>
2017-12-19 21:27:13 +0800

    添加http

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import router from './router'
 import 'assets/css/base.scss'
 import 'assets/css/icon.scss'
 import fastclick from 'fastclick'
+import {AjaxPlugin} from 'vux'
 
 fastclick.attach(document.body)
 Vue.config.productionTip = false
+Vue.use(AjaxPlugin)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
