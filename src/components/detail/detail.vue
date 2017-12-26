77e097abc36cb06ddfac45d178b54c3cc24abec2
tianhuifeis <tianhuifeis@126.com>
2017-12-26 21:47:26 +0800

    测试路由

---
+++ src/components/detail/detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="detail-main">
+    <h1>我是detail</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'detail'
+  }
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/css/base-standard';
+  .detail-main{
+    position: fixed;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+    z-index: 100;
+    background:$app-bg-color;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'components/home/home'
+import Detail from 'components/detail/detail'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
