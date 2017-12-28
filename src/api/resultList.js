1d5e61e294f11e68ebaadd3364d97a907437129d
tianhuifeis <tianhuifeis@126.com>
2017-12-28 17:06:14 +0800

    新增接口

--- src/api/resultList.js
+++ src/api/resultList.js
@@ -1,15 +1,25 @@
 // import axios from 'axios'
 import jsonp from 'common/js/jsonp'
 import {options} from './config'
-
-// import {ROOT_URL} from 'common/js/base'
+import {ROOT_URL} from 'common/js/base'
 
 export function getQueryALl() {
-  const url = 'http://localhost:8360/index/index/query_all'
+  const url = ROOT_URL + '/index/index/query_all'
   const data = {
     'condition.currentPage': 0,
     'condition.numsPerPage': 5
   }
 
+  return jsonp(url, data, options)
+}
+
+export function queryStartEnd(startAdd, endAdd) {
+  const url = ROOT_URL + '/index/index/query_start_end'
+  const data = {
+    'condition.currentPage': 0,
+    'condition.numsPerPage': 5,
+    'condition.startAddress': startAdd,
+    'condition.endAddres': endAdd
+  }
   return jsonp(url, data, options)
 }
