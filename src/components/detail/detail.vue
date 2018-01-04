19a5f9a2a91f16dee9da86f898fcdf5d3d403135
tianhuifeis <tianhuifeis@126.com>
2018-01-04 22:13:38 +0800

    修改页面样式

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -1,32 +1,25 @@
 <template>
-  <transition name="detail">
-    <div class="detail-main">
-      <h1>我是detail</h1>
+  <transition name="page">
+    <div class="page-style">
+      <x-header>详细信息</x-header>
+      <div class="detail-main">
+        <h1>我是detail</h1>
+      </div>
     </div>
+
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import {XHeader} from 'vux'
   export default {
-    name: 'detail'
+    name: 'detail',
+    components: {
+      XHeader
+    }
   }
 </script>
 
 <style scoped lang="scss">
   @import '../../assets/css/base-standard';
-  .detail-main{
-    position: fixed;
-    top:0;
-    bottom:0;
-    left:0;
-    right:0;
-    z-index: 100;
-    background:$app-bg-color;
-  }
-  .detail-enter-active, .detail-leave-active {
-    transition: all .3s
-  }
-  .detail-enter, .detail-leave-to /* .fade-leave-active in below version 2.1.8 */ {
-    transform: translate3d(100%, 0, 0)
-  }
 </style>
