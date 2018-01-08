8a02c798bbe84c4abd9322cdce791db350b2c3d5
tianhuifeis <tianhuifeis@126.com>
2018-01-08 09:59:21 +0800

    增加xheader

--- src/components/publish-edit/publish-edit.vue
+++ src/components/publish-edit/publish-edit.vue
@@ -1,16 +1,39 @@
 <template>
   <transition name="page">
-  <div class=" page-style">
-    <div class="publish-main">
+    <div class=" page-style">
+      <x-header>{{title}}</x-header>
+      <div class="publish-main">
+      </div>
     </div>
-  </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import {XHeader} from 'vux'
+
   export default {
     name: 'publish-edit',
-    components: {}
+    data() {
+      return {
+        title: '',
+        id: '',
+        type: ''
+      }
+    },
+    components: {
+      XHeader
+    },
+
+    mounted() {
+      this._initPublish()
+    },
+    methods: {
+      _initPublish() {
+        this.title = this.$route.params.id === '-1' ? '发布' : '编辑'
+        this.id = this.$route.params.id
+        this.type = this.$route.params.type
+      }
+    }
   }
 </script>
 
