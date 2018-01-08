68023edfe0d607e227c5eb72b0aa47640bc65919
tianhuifeis <tianhuifeis@126.com>
2018-01-08 16:15:38 +0800

    增加toast组件

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import router from './router'
 import 'assets/css/base.scss'
 import 'assets/css/icon.scss'
 import fastclick from 'fastclick'
+import {ToastPlugin} from 'vux'
 
 fastclick.attach(document.body)
 Vue.config.productionTip = false
+Vue.use(ToastPlugin, {position: 'top'})
 
 /* eslint-disable no-new */
 new Vue({
