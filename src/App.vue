e0df527ac2497e49789d9917c8f62153ba1c7d6f
tianhuifeis <tianhuifeis@126.com>
2018-01-09 15:55:15 +0800

    tabbar位置修改

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,14 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
-    <tab-bar></tab-bar>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import TabBar from 'components/tab-bar/tab-bar'
   import Home from 'components/home/home'
 
   export default {
     components: {
-      TabBar,
       Home
     }
   }
