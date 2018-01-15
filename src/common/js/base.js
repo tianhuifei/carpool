da735faff6b3a3c1871015f6254f1b08f4076aa1
tianhuifeis <tianhuifeis@126.com>
2018-01-15 17:07:25 +0800

    增加类型检测

--- src/common/js/base.js
+++ src/common/js/base.js
@@ -1,2 +1,6 @@
 // export const ROOT_URL = 'https://www.tianhf.com'
 export const ROOT_URL = 'http://localhost:8360'
+
+export function isType(value) {
+  return Object.prototype.toString.call(value)
+}
