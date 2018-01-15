78169ea7e4c55f4afdb94190071cc91e2b170415
tianhuifeis <tianhuifeis@126.com>
2018-01-15 17:06:37 +0800

    发布

--- src/api/publish-edit.js
+++ src/api/publish-edit.js
@@ -9,3 +9,23 @@ export function presentTime() {
   let url = '/index/index/present_time'
   return jsonp(ROOT_URL + url, null, options)
 }
+
+export function addPublish(carpoolInfo) {
+  let url = '/publish/publish/add_publish'
+  const data = {
+    'publish.publishType': carpoolInfo.publishType,
+    'publish.startAddress': carpoolInfo.startAddress,
+    'publish.endAddres': carpoolInfo.endAddres,
+    'publish.contacts': carpoolInfo.contacts,
+    'publish.contactsPhone': carpoolInfo.contactsPhone,
+    'publish.carNumber': carpoolInfo.carNumber,
+    'publish.vacancy': carpoolInfo.vacancy,
+    'publish.remarks': carpoolInfo.remarks,
+    'publish.startDate': carpoolInfo.startDate,
+    'publish.startTime': carpoolInfo.startTime,
+    'publish.openId': carpoolInfo.openId,
+    'publish.publishId': carpoolInfo.publishId,
+    'publish.publishTime': carpoolInfo.publishTime
+  }
+  return jsonp(ROOT_URL + url, data, options)
+}
--- src/components/publish-edit/publish-edit.vue
+++ src/components/publish-edit/publish-edit.vue
@@ -10,9 +10,12 @@
                             @on-change="setPublishType"></popup-picker>
             </group>
             <group gutter="10px" label-width="5.5em" label-margin-right="1em" label-align="justify">
-              <x-input v-model="carpoolInfo.startAddress" :required="true" title="出发地：" placeholder="请输入出发地"></x-input>
-              <x-input v-model="carpoolInfo.endAddres" :required="true" title="目的地：" placeholder="请输入目的地"></x-input>
-              <datetime @on-change="setDateTime" :required="true" :title="publishObj.setDateTitle"
+              <x-input :is-type="isStartAddress" v-model="carpoolInfo.startAddress" :required="true" title="出发地："
+                       placeholder="请输入出发地"></x-input>
+              <x-input :is-type="isEndAddress" v-model="carpoolInfo.endAddres" :required="true" title="目的地："
+                       placeholder="请输入目的地"></x-input>
+              <datetime @on-change="setDateTime" :required="true"
+                        :title="publishObj.setDateTitle"
                         format="YYYY-M-DD HH:mm"
                         :start-date="publishObj.startDate" :end-date="publishObj.endDate" year-row="{value}年"
                         month-row="{value}月" day-row="{value}日" hour-row="{value}点" minute-row="{value}分"
@@ -21,12 +24,12 @@
             <group gutter="10px" label-width="5.5em" label-margin-right="2em" label-align="justify">
               <x-input is-type="china-name" type="text" :required="true" title="联系人：" placeholder="请输入联系人"
                        v-model="carpoolInfo.contacts"></x-input>
-              <x-input mask="999 9999 9999" type="tel" is-type="china-mobile" :required="true" title="手机号："
+              <x-input mask="999 9999 9999" type="tel" :max="13" :is-type="isPhone" :required="true" title="手机号："
                        v-model="carpoolInfo.contactsPhone"
                        placeholder="请输入手机号"></x-input>
               <x-input v-if="!carpoolInfo.publishType" title='车牌号：' v-model="carpoolInfo.carNumber" type="text"
                        placeholder="请输入车牌号"></x-input>
-              <x-input title='人数/空位：' type="number" :required="true" v-model="carpoolInfo.vacancy" :max="2"
+              <x-input :max="2" title='人数/空位：' type="number" :required="true" v-model="carpoolInfo.vacancy"
                        placeholder="人数/空位"></x-input>
             </group>
             <group title="备注">
@@ -38,7 +41,7 @@
               <a @click="showDisclaimer" href="javascript:void(0)">《免责声明》</a>
             </div>
             <div class="" style="padding:5px 10px;padding-bottom:15px;">
-              <x-button :disabled="!publishObj.submitBtn" type="primary">立即发布</x-button>
+              <x-button @click.native="submitPublish" :disabled="!publishObj.submitBtn" type="primary">立即发布</x-button>
             </div>
           </div>
         </scroll>
@@ -51,8 +54,9 @@
 <script type="text/ecmascript-6">
   import {XHeader, Group, Cell, PopupPicker, XInput, Datetime, XTextarea, CheckIcon, XButton} from 'vux'
   import Scroll from 'base/scroll/scroll'
-  import {presentTime} from '../../api/publish-edit'
+  import {presentTime, addPublish} from '../../api/publish-edit'
   import disclaimer from 'components/disclaimer/disclaimer'
+  import {isType} from '../../common/js/base'
 
   export default {
     name: 'publish-edit',
@@ -60,13 +64,16 @@
       return {
         showHideOnBlur: true,
         publishObj: {
+          checkPhone: false,
+          checkStartAdd: false,
+          checkEndAdd: false,
           submitBtn: false,
           disclaimer: false,
           publishTitle: '拼车类型：',
           publishValue: [],
           setDateTitle: '出发时间:',
-          startDate: '2018-01-11',
-          endDate: '2018-12-31',
+          startDate: '',
+          endDate: '',
           liabilityTitle: '',
           liability: false
         },
@@ -106,10 +113,23 @@
     },
     computed: {},
     mounted() {
+    },
+    activated() {
       this._initPublish()
       this._getDate()
     },
     methods: {
+      submitPublish() {
+        if (event._constructed) {
+          return
+        }
+        if (!this._checkCondition()) {
+          return false
+        }
+        addPublish(this.carpoolInfo).then((res) => {
+          console.log(res)
+        }).catch().finally()
+      },
       showDisclaimer() {
         this.publishObj.disclaimer = true
       },
@@ -124,6 +144,31 @@
         this.carpoolInfo.startDate = dataObj[0]
         this.carpoolInfo.startTime = dataObj[1]
       },
+      isPhone(value) {
+        value = value.replace(/\s/g, '')
+        let phoneRex = /^134[0-8]\d{7}$|^13[^4]\d{8}$|^14[5-9]\d{8}$|^15[^4]\d{8}$|^16[6]\d{8}$|^17[0-8]\d{8}$|^18[\d]{9}$|^19[8,9]\d{8}$/
+        this.publishObj.checkPhone = phoneRex.test(value)
+        return {
+          valid: this.publishObj.checkPhone,
+          msg: '请输入正确手机号'
+        }
+      },
+      isStartAddress(value) {
+        let addressRex = /^[\u4e00-\u9fa5A]{2,}/
+        this.publishObj.checkStartAdd = addressRex.test(value)
+        return {
+          valid: this.publishObj.checkStartAdd,
+          msg: '请输入有效的地址'
+        }
+      },
+      isEndAddress(value) {
+        let addressRex = /^[\u4e00-\u9fa5A]{2,}/
+        this.publishObj.checkEndAdd = addressRex.test(value)
+        return {
+          valid: this.publishObj.checkEndAdd,
+          msg: '请输入有效的地址'
+        }
+      },
       _initPublish() {
         this.title = this.$route.params.id === '-1' ? '发布' : '编辑'
         this.id = this.$route.params.id
@@ -131,10 +176,65 @@
       },
       _getDate() {
         presentTime().then((res) => {
-          console.log(res)
+          let dateObj = res.result
+          this.publishObj.startDate = dateObj.dateObj.start
+          this.publishObj.endDate = dateObj.dateObj.end
         }).catch().finally(() => {
-          console.log('都会执行')
+
         })
+      },
+      _checkCondition() {
+        if (this.carpoolInfo.publishType === '') {
+          this.$vux.toast.show({
+            type: 'warn',
+            text: '请选择拼车类型'
+          })
+          return false
+        }
+        if (!this.publishObj.checkStartAdd) {
+          this.$vux.toast.show({
+            type: 'warn',
+            text: '请输入出发地'
+          })
+          return false
+        }
+        if (!this.carpoolInfo.startDate) {
+          this.$vux.toast.show({
+            type: 'warn',
+            text: '请输入出发日期'
+          })
+          return false
+        }
+        if (!this.publishObj.checkEndAdd) {
+          this.$vux.toast.show({
+            type: 'warn',
+            text: '请输入目的地'
+          })
+          return false
+        }
+        if (!this.carpoolInfo.contacts) {
+          this.$vux.toast.show({
+            type: 'warn',
+            text: '请输入联系人'
+          })
+          return false
+        }
+        if (!this.publishObj.checkPhone) {
+          this.$vux.toast.show({
+            type: 'warn',
+            text: '请输入联系方式'
+          })
+          return false
+        }
+        console.log(isType(this.carpoolInfo.vacancy))
+        if (!this.carpoolInfo.vacancy) {
+          this.$vux.toast.show({
+            type: 'warn',
+            text: '请输入人数/空位'
+          })
+          return false
+        }
+        return true
       }
     }
   }
