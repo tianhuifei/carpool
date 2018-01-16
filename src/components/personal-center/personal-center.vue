9ea87158eff6137dc012ddb45c24d94bcac61fe2
tianhuifeis <tianhuifeis@126.com>
2018-01-16 16:17:33 +0800

    个人中心

---
+++ src/components/personal-center/personal-center.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="body-main">
+    <div class="my-header">
+      <p class="center">
+        <img :src="url" alt="">
+      </p>
+    </div>
+    <group gutter="5px">
+      <cell title="我的发布"  is-link><span slot="icon" class="carpoll car-fabu icon-color"></span></cell>
+      <cell title="关于" is-link><span slot="icon" class="carpoll car-guanyu icon-color"></span></cell>
+    </group>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {Group, Cell} from 'vux'
+
+  export default {
+    name: 'personal-center',
+    data() {
+      return {
+        url: 'https://o3e85j0cv.qnssl.com/tulips-1083572__340.jpg'
+      }
+    },
+    components: {
+      Group,
+      Cell
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "../../assets/css/base-standard";
+
+  .my-header{
+    display:flex;
+    height:0;
+    padding-top:60%;
+    position:relative;
+    align-items: center;
+    justify-items: center;
+    background-color:$app-color;
+    .center{
+      position: absolute;
+      top:50%;
+      left:50%;
+      transform: translate(-50%,-50%);
+      img{
+        width:100px;
+        height:100px;
+        border-radius: 50%;
+        border: 2px solid $color-white;
+        box-shadow:0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12);
+      }
+    }
+  }
+  .icon-color{
+    color:$app-color;
+    font-size:16px;
+    margin-right:5px;
+  }
+</style>
