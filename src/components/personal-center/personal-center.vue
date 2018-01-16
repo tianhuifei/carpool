49396fbf32fd8ab64ee2de53525f509283b8e7ff
tianhuifeis <tianhuifeis@126.com>
2018-01-16 17:24:25 +0800

    增加activated生命周期函数

--- src/components/personal-center/personal-center.vue
+++ src/components/personal-center/personal-center.vue
@@ -6,7 +6,8 @@
       </p>
     </div>
     <group gutter="5px">
-      <cell title="我的发布"  is-link link="/personal/1123"><span slot="icon" class="carpoll car-fabu icon-color"></span></cell>
+      <cell title="我的发布" is-link link="/personal/1123"><span slot="icon" class="carpoll car-fabu icon-color"></span>
+      </cell>
       <cell title="关于" is-link><span slot="icon" class="carpoll car-guanyu icon-color"></span></cell>
     </group>
     <router-view></router-view>
@@ -23,6 +24,8 @@
         url: 'https://o3e85j0cv.qnssl.com/tulips-1083572__340.jpg'
       }
     },
+    activated() {
+    },
     components: {
       Group,
       Cell
@@ -33,31 +36,32 @@
 <style scoped lang="scss">
   @import "../../assets/css/base-standard";
 
-  .my-header{
-    display:flex;
-    height:0;
-    padding-top:60%;
-    position:relative;
+  .my-header {
+    display: flex;
+    height: 0;
+    padding-top: 60%;
+    position: relative;
     align-items: center;
     justify-items: center;
-    background-color:$app-color;
-    .center{
+    background-color: $app-color;
+    .center {
       position: absolute;
-      top:50%;
-      left:50%;
-      transform: translate(-50%,-50%);
-      img{
-        width:100px;
-        height:100px;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      img {
+        width: 100px;
+        height: 100px;
         border-radius: 50%;
         border: 2px solid $color-white;
-        box-shadow:0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12);
+        box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .16), 0 2px 10px 0 rgba(0, 0, 0, .12);
       }
     }
   }
-  .icon-color{
-    color:$app-color;
-    font-size:16px;
-    margin-right:5px;
+
+  .icon-color {
+    color: $app-color;
+    font-size: 16px;
+    margin-right: 5px;
   }
 </style>
