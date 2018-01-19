4b2ea5774d777d299199f45493d32773149f8756
tianhuifeis <tianhuifeis@126.com>
2018-01-19 15:26:22 +0800

    修改样式

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,43 +1,25 @@
 <template>
   <div class="login">
-    <div class="input-box" ref="inputBox">
-      <div class="input-title">
-        请输入用户名
-      </div>
-      <input type="text" @blur="inputBlur" @focus="inputFocus" v-model="currentValue">
-      <span class="input-border-bottom"></span>
-      <span class="input-border-bottom-focus"></span>
-    </div>
+    <t-input type="text" title="请输入用户名" v-model="inputValue"></t-input>
+    <t-input type="password" title="请输入密码" v-model="inputValue"></t-input>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {addClass, removeClass} from 'common/js/dom'
+  import TInput from 'components/T-input/T-input'
 
   export default {
     name: 'login',
     data() {
       return {
-        currentValue: ''
+        inputValue: '一二三四五'
       }
     },
-    activated() {
-    },
-    methods: {
-      inputFocus() {
-        addClass(this.$refs.inputBox, 'focus')
-      },
-      inputBlur() {
-        removeClass(this.$refs.inputBox, 'focus')
-      }
+    components: {
+      TInput
     },
     watch: {
-      currentValue(val) {
-        if (val) {
-          addClass(this.$refs.inputBox, 'has-label')
-        } else {
-          removeClass(this.$refs.inputBox, 'has-label')
-        }
+      inputValue(val) {
       }
     }
   }
@@ -59,68 +41,5 @@
   }
 
   /******/
-  .input-box {
-    padding: 28px 10px 12px 10px;
-    position: relative;
-    text-align: left;
-    input {
-      appearance: none;
-      outline: none;
-      border: none;
-      background: none;
-      border-radius: 0 0 0 0;
-      box-shadow: none;
-      display: block;
-      padding: 0;
-      margin: 0;
-      width: 100%;
-      height: 32px;
-    }
-    .input-border-bottom {
-      height: 1px;
-      border: none;
-      margin: 0;
-      background-color: rgba(0, 0, 0, .12);
-      position: absolute;
-      left: 0;
-      right: 0;
-    }
-    .input-border-bottom-focus {
-      margin-top: -1px;
-      height: 2px;
-      border: none;
-      background-color: $app-color;
-      position: absolute;
-      left: 0;
-      right: 0;
-      transform: scaleX(0);
-      transition: transform .45s ease;
-    }
-    .input-title {
-      display: inline-block;
-      font-size: 14px;
-      color: $basic-font-color;
-      transition: all .45s ease;
-      cursor: text;
-      transform-origin: left top;
-      user-select: none;
-      pointer-events: none;
-      transform: translate3d(0, 23px, 0) scale(1);
-    }
-    &.focus {
-      .input-border-bottom-focus {
-        transform: scaleX(1);
-      }
-      .input-title {
-        transform: translateZ(0) scale(.75);
-        color: $app-color !important;
-      }
-    }
-    &.has-label {
-      .input-title {
-        transform: translateZ(0) scale(.75);
-        color: $basic-font-color;
-      }
-    }
-  }
+
 </style>
