3466a925b71b222b1cdc3e5b4a9fbefd85f4989f
tianhuifeis <tianhuifeis@126.com>
2018-01-23 18:09:56 +0800

    登录与注册切换

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="login">
-    <div class="logo-img">
-      <img src="/static/images/logo.png" alt="">
+  <div class="login" :class="{'register-active':register }" ref="loginBox">
+    <div class="side front">
+      <div class="register" @click="onLoginOrReg">
+        <a href="javascript:void(0);">注册</a>
+      </div>
+      <div class="logo-img">
+        <img src="/static/images/logo.png" alt="">
+      </div>
+      <t-input type="text" title="请输入用户名" v-model="userName"></t-input>
+      <t-input type="password" title="请输入密码" v-model="passWord"></t-input>
+      <div class="btn-box">
+        <x-button type="primary" :plain="true" :mini="true">登录</x-button>
+      </div>
     </div>
-    <t-input type="text" title="请输入用户名" v-model="userName"></t-input>
-    <t-input type="password" title="请输入密码" v-model="passWord"></t-input>
-    <div class="btn-box">
-      <x-button type="primary" :plain="true" :mini="true">登录</x-button>
+    <div class="side back">
+      <div class="register" @click="onLoginOrReg">
+        <a href="javascript:void(0);">登录</a>
+      </div>
+      这是反而
     </div>
 
   </div>
@@ -21,12 +32,19 @@
     data() {
       return {
         userName: '',
-        passWord: ''
+        passWord: '',
+        register: false
       }
     },
     components: {
       TInput,
       XButton
+    },
+    methods: {
+      onLoginOrReg() {
+        this.register = !this.register
+        console.log(this.register)
+      }
     },
     watch: {
       inputValue(val) {
@@ -47,22 +65,69 @@
   }
 
   .login {
+    position: relative;
     height: 300px;
   }
 
-  .logo-img{
-    margin-top:10%;
-    width:100px;
-    height:100px;
-    text-align: center;
-    margin: 0 auto;
-    img{
-      width:100%;
-      height:100%;
+  .login-fixed, .login {
+    perspective: 1000px;
+    transform-style: preserve-3d;
+  }
+
+  .register {
+    padding:5px;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    color: $basic-font-color;
+    z-index: 10;
+  }
+
+  .side {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    top: 0;
+    transition: all .8s ease;
+    backface-visibility: hidden;
+    background-color: $color-white;
+  }
+
+  .front {
+    transform: rotateY(0deg);
+    z-index: 1;
+  }
+
+  .back {
+    transform: rotateY(180deg);
+    z-index: 0;
+  }
+
+  .register-active {
+    .front {
+      transform: rotateY(180deg) ;
+    }
+
+    .back {
+      transform: rotateY(360deg) ;
     }
   }
-  .btn-box{
-    margin-top:18px;
+
+  .logo-img {
+    margin-top: 10%;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    margin: 0 auto;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .btn-box {
+    margin-top: 18px;
   }
   /******/
 
