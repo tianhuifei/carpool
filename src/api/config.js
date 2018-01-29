93528e85200aba9ee20fb0b55503f0f0d369d7c6
tianhuifeis <tianhuifeis@126.com>
2018-01-29 16:55:05 +0800

    增加注册url

--- src/api/config.js
+++ src/api/config.js
@@ -4,6 +4,8 @@ export const carUrl = '/index/index/query_ctop'
 export const peopleUrl = '/index/index/query_ptoc'
 export const queryAll = '/index/index/query_all'
 export const startEndUrl = '/index/index/query_start_end'
+// 注册
+export const registerUrl = '/user/index/add_user_info'
 export const options = {
   param: 'callback'
 }
