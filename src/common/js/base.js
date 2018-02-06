0463cbc4fd50003ab6dd18609efd7ffc2927b279
tianhuifeis <tianhuifeis@126.com>
2018-02-06 21:58:45 +0800

    增加获取用户信息方法

--- src/common/js/base.js
+++ src/common/js/base.js
@@ -1,6 +1,18 @@
+import {getCookie} from './cookie'
 // export const ROOT_URL = 'https://www.tianhf.com'
 export const ROOT_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8360'
 
 export function isType(value) {
   return Object.prototype.toString.call(value)
+}
+
+/**
+ * 获取用户信息
+ * @returns {*}
+ */
+export function getUserInfo() {
+  let user = JSON.parse(localStorage.getItem('user'))
+  if (user) return user
+  let cookieUser = JSON.parse(getCookie('userInfo'))
+  if (cookieUser) return cookieUser
 }
