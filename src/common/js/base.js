12e76e9c21086bcb6e6893c31b4a7b93f1e6b805
tianhuifeis <tianhuifeis@126.com>
2018-03-02 16:07:26 +0800

    增加格式化时间方法

--- src/common/js/base.js
+++ src/common/js/base.js
@@ -14,3 +14,25 @@ export function getUserInfo() {
   let user = JSON.parse(localStorage.getItem('user')) || JSON.parse(getCookie('userInfo') ? getCookie('userInfo') : null)
   return user
 }
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+/**
+ * 格式化时间
+ * @param date
+ * @returns {string}
+ */
+export function formatTime(date) {
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+
+  let hour = date.getHours()
+  let minute = date.getMinutes()
+  let second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
