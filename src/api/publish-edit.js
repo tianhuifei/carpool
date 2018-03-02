5e326e98480c49b4a23f48da5506890112cd7197
tianhuifeis <tianhuifeis@126.com>
2018-03-02 16:08:33 +0800

    增加更新方法

--- src/api/publish-edit.js
+++ src/api/publish-edit.js
@@ -1,6 +1,6 @@
 import jsonp from 'common/js/jsonp'
 import {ROOT_URL} from 'common/js/base'
-import {isProduction, options, queryEditUrl} from './config'
+import {isProduction, options, queryEditUrl, updateInfoUrl} from './config'
 import axios from 'axios'
 
 /**
@@ -49,6 +49,34 @@ export function queryEdit(id) {
   let url = ROOT_URL + queryEditUrl
   let data = {
     'condition.publishId': id
+  }
+  if (isProduction) {
+    return axios.post(url, data)
+  }
+  return jsonp(url, data, options)
+}
+
+/**
+ * 更新已发布的数据
+ * @param carpoolInfo
+ * @returns {AxiosPromise<any>}
+ */
+export function updateInfo(carpoolInfo) {
+  let url = ROOT_URL + updateInfoUrl
+  const data = {
+    'publish.publishType': carpoolInfo.publishType,
+    'publish.startAddress': carpoolInfo.startAddress,
+    'publish.endAddres': carpoolInfo.endAddres,
+    'publish.contacts': carpoolInfo.contacts,
+    'publish.contactsPhone': carpoolInfo.contactsPhone,
+    'publish.carNumber': carpoolInfo.carNumber,
+    'publish.vacancy': carpoolInfo.vacancy,
+    'publish.remarks': carpoolInfo.remarks,
+    'publish.startDate': carpoolInfo.startDate,
+    'publish.startTime': carpoolInfo.startTime,
+    'publish.openId': carpoolInfo.openId,
+    'publish.publishId': carpoolInfo.publishId,
+    'publish.publishTime': carpoolInfo.publishTime
   }
   if (isProduction) {
     return axios.post(url, data)
