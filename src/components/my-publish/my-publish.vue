143c4e37f811d383925295d38498c37ce96020b8
tianhuifeis <tianhuifeis@126.com>
2018-04-24 09:24:34 +0800

    修改BUG

--- src/components/my-publish/my-publish.vue
+++ src/components/my-publish/my-publish.vue
@@ -157,15 +157,15 @@
         } else {
           this.list = []
         }
-      },
-      beforeRouteEnter(to, from, next) {
-        next(vm => {
-          if (from.name === 'Publish' && from.params.type === 'edit' && from.meta.refresh) {
-            setTimeout(vm._loadData(), 500)
-            from.meta.refresh = false
-          }
-        })
       }
+    },
+    beforeRouteEnter(to, from, next) {
+      next(vm => {
+        if (from.name === 'Publish' && from.params.type === 'edit' && from.meta.refresh) {
+          setTimeout(vm._loadData(true), 500)
+          from.meta.refresh = false
+        }
+      })
     }
   }
 </script>
