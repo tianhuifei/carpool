bce961f5b6b942a10736e58da900eb3d8871ce49
tianhuifeis <tianhuifeis@126.com>
2018-05-27 16:43:51 +0800

    测试异步路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import TLoading from '../base/T-loading/T-loading'
+import TError from '../base/T-error/T-error'
 
-const Home = () => import('components/home/home')
 const Detail = () => import('components/detail/detail')
 const PublishEdit = () => import('components/publish-edit/publish-edit')
 const PersonalCenter = () => import('components/personal-center/personal-center')
@@ -10,6 +11,19 @@ const Version = () => import('components/version/version')
 const FeedBack = () => import('components/feed-back/feed-back')
 const contactMe = () => import('components/contact-me/contact-me')
 const About = () => import('components/about/about')
+const Home = () => ({
+  // 需要加载的组件 (应该是一个 `Promise` 对象)
+  component: import('components/home/home'),
+  // 异步组件加载时使用的组件
+  loading: TLoading,
+  // 加载失败时使用的组件
+  error: TError,
+  // 展示加载时组件的延时时间。默认值是 200 (毫秒)
+  delay: 200,
+  // 如果提供了超时时间且组件加载也超时了，
+  // 则使用加载失败时使用的组件。默认值是：`Infinity`
+  timeout: 3000
+})
 
 Vue.use(Router)
 let router = new Router({
