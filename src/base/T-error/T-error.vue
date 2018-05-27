797f6269426fa27fcf09522dfd45784148fcd03c
tianhuifeis <tianhuifeis@126.com>
2018-05-27 17:07:58 +0800

    增加错误页面

--- src/base/T-error/T-error.vue
+++ src/base/T-error/T-error.vue
@@ -2,7 +2,7 @@
   <div class="error-box">
 
     <div class="error-text">
-      这里发生了一些错误，我正在查看。请不要着急。
+      这里发生了一些错误。
     </div>
 
 
@@ -18,14 +18,20 @@
 <style scoped lang="scss">
   @import "../../assets/css/base-standard";
   .error-box{
-    display:flex;
     position:fixed;
     top:0;
     left:0;
     right:0;
     bottom:0;
+    z-index: 99;
     .error-text{
+      position:absolute;
+      left:50%;
+      top:40%;
+      transform: translate(-40%,-50%);
       font-size: 14px;
+      color:$basic-font-color;
+      line-height: 1.5;
       @include justify-items
     }
   }
